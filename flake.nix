c31b1e55f28e3cbb67671332e59e039b9e116e04
= Alex Eisenschmied <105024964+Schmiedium@users.noreply.github.com>
2025-05-17 19:38:01 -0400

    updated home manager url and I think determinate nix should be properly configured now

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -18,13 +18,13 @@
     nixosConfigurations = {
       nixos = lib.nixosSystem {
         inherit system;
-        modules = [./configuration.nix];
+        modules = [./configuration.nix determinate.nixosModules.default];
       };
     };
     homeConfigurations = {
       alex = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [./home.nix determinate.nixosModules.default];
+        modules = [./home.nix];
       };
     };
   };
