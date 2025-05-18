198366047a7a1523ea39d1b8d6b546e282a15bb7
= Alex Eisenschmied <105024964+Schmiedium@users.noreply.github.com>
2025-05-18 11:07:21 -0400

    fonts one last time

--- configuration.nix
+++ configuration.nix
@@ -193,7 +193,7 @@
 
 
   fonts.packages = with pkgs; [
-     nerd-fonts.hasklug nerd-fonts.firacode  ];
+     nerd-fonts.hasklug nerd-fonts.fira-code  ];
 
   nixpkgs.config.permittedInsecurePackages = [
     "electron-25.9.0"
