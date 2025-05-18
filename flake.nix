39e634a4fa97869142c04cced48f6c82fa0bb68c
= Alex Eisenschmied <105024964+Schmiedium@users.noreply.github.com>
2025-05-18 11:30:36 -0400

    "not sure I was properly on 25.05"

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
