22176b401eaad2beaf7f7ea5aec233fe8a511701
= Alex Eisenschmied <105024964+Schmiedium@users.noreply.github.com>
2025-05-30 19:17:49 -0400

    Adding u2fAuth for yubikey login

--- configuration.nix
+++ configuration.nix
@@ -84,6 +84,10 @@
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+  };
   services.pipewire = {
     enable = true;
     alsa.enable = true;
