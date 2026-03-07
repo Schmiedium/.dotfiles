f11267832a4adf4352fb6a4e3c4f630267a223cb
= Alex Eisenschmied <105024964+Schmiedium@users.noreply.github.com>
2026-03-07 14:10:58 -0500

    "trying again"

--- configuration.nix
+++ configuration.nix
@@ -170,7 +170,6 @@
   starship
   rustup
   clang
-  claude-code
   gcc
   neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   alacritty
