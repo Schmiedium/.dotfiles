4b0cdd7c95c62617e67f7a3d891bd0fbf8b30b1f
= Alex Eisenschmied <105024964+Schmiedium@users.noreply.github.com>
2026-04-10 19:01:29 -0400

    modularized nix setup

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     import-tree.url = "github:vic/import-tree";
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     claude-code.url = "github:sadjow/claude-code-nix";
   };
